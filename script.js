66db5792bb9aab5243cb51656a35cd9da3743c92
ayushkothari1 <pahadiayush61@gmail.com>
2025-01-21 12:04:05 +0530

    later bettement

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const selectField = document.querySelector("#selectField");
 const selectText = document.querySelector("#selectText");
 const options = document.querySelectorAll(".options");
 const list = document.querySelector("#list");
+const arrowIcon = document.querySelector("#arrowIcon");
 
-selectField.onclick = function(){
+selectField.addEventListener("click", function(){
     list.classList.toggle("hide");
-}
+    arrowIcon.classList.toggle("rotate");
+})
 for (const option of options) {
-  option.onclick = function () {
+  option.addEventListener("click", function () {
     selectText.innerHTML = this.textContent;
-  };
+    list.classList.toggle("hide");
+   arrowIcon.classList.toggle("rotate");
+  });
 }
